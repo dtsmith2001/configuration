067a7db04b316b2c3d65396f3fcd00a8fee6471e
Dale Smith <dtsmith@mindspring.com>
2018-09-16 12:00:16 -0400

    add .ssh-agent link

--- bin/make_links.sh
+++ bin/make_links.sh
@@ -6,33 +6,37 @@ cd ${HOME}
 
 if [ ! -f .aliases ]
 then
-    ln -s ${HOME}/src/configuration/common/.aliases .aliases
+    ln -s ${HOME}/src/Vallum_DevOps/employees/dtsmith/.aliases .aliases
 fi
 if [ ! -f .bash_functions ]
 then
-    ln -s ${HOME}/src/configuration/common/.bash_functions .bash_functions
+    ln -s ${HOME}/src/Vallum_DevOps/employees/dtsmith/.bash_functions .bash_functions
 fi
 if [ ! -f .bash_profile ]
 then
-    ln -s ${HOME}/src/configuration/common/.bash_profile .bash_profile
+    ln -s ${HOME}/src/Vallum_DevOps/employees/dtsmith/.bash_profile .bash_profile
 fi
 if [ ! -f .bashrc ]
 then
-    ln -s ${HOME}/src/configuration/common/.bashrc .bashrc
+    ln -s ${HOME}/src/Vallum_DevOps/employees/dtsmith/.bashrc .bashrc
 fi
 if [ ! -f .gitconfig ]
 then
-    ln -s ${HOME}/src/configuration/common/.gitconfig .gitconfig
+    ln -s ${HOME}/src/Vallum_DevOps/employees/dtsmith/.gitconfig .gitconfig
 fi
 if [ ! -f .my_prompt ]
 then
-    ln -s ${HOME}/src/configuration/common/.my_prompt .my_prompt
+    ln -s ${HOME}/src/Vallum_DevOps/employees/dtsmith/.my_prompt .my_prompt
 fi
 if [ ! -f .gdbinit ]
 then
-    ln -s ${HOME}/src/configuration/common/.gdbinit .gdbinit
+    ln -s ${HOME}/src/Vallum_DevOps/employees/dtsmith/.gdbinit .gdbinit
 fi
 if [ ! -f .additional_startup ]
 then
-    ln -s ${HOME}/src/configuration/common/.additional_startup .additional_startup
+    ln -s ${HOME}/src/Vallum_DevOps/employees/dtsmith/.additional_startup .additional_startup
+fi
+if [ ! -f .ssh_agent ]
+then
+    ln -s ${HOME}/src/Vallum_DevOps/employees/dtsmith/.ssh_agent .ssh_agent
 fi
